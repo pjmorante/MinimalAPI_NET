955c6e25f36811ca9be2cae82ee29d5c603bf906
Pedro Morante <pedro.morante@outlook.com>
2023-01-25 09:49:08 -0500

    Add endpoints name

--- MinimalAPIDemo/MagicVilla_CouponAPI/Program.cs
+++ MinimalAPIDemo/MagicVilla_CouponAPI/Program.cs
@@ -1,3 +1,6 @@
+using MagicVilla_CouponAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,6 +16,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapGet("/api/coupon", () => {
+    return Results.Ok();
+}).WithName("GetCoupons");
+
+app.MapGet("/api/coupon/{id:int}", (int id) => {
+    return Results.Ok(FirstOrDefault(u => u.Id == id);
+}).WithName("GetCoupon");
+
+app.MapPost("/api/coupon", ([FromBody] Coupon coupon) => {
+    if(coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+    {
+        return Results.BadRequest("Invalis Id or Coupon name");
+    }
+    if(CouponStore.)
+});
 
 app.UseHttpsRedirection();
 
