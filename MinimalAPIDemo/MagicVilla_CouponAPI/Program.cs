51d44ee5c45de1e236538c6f2caceb834a14328d
Pedro Morante <pedro.morante@outlook.com>
2023-01-25 11:48:21 -0500

    Add DTOs folder

--- MinimalAPIDemo/MagicVilla_CouponAPI/Program.cs
+++ MinimalAPIDemo/MagicVilla_CouponAPI/Program.cs
@@ -1,4 +1,5 @@
 using MagicVilla_CouponAPI.Models;
+using MagicVilla_CouponAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -17,21 +18,45 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/api/coupon", () => {
+app.MapGet("/api/coupon", (ILogger<Program> _logger) => {
+    _logger.Log(LogLevel.Information, "Getting all coupons");
     return Results.Ok();
-}).WithName("GetCoupons");
+}).WithName("GetCoupons").Produces<IEnumerable<Coupon>>(200);
 
-app.MapGet("/api/coupon/{id:int}", (int id) => {
-    return Results.Ok(FirstOrDefault(u => u.Id == id);
-}).WithName("GetCoupon");
+//app.MapGet("/api/coupon/{id:int}", (int id) => {
+//    return Results.Ok(FirstOrDefault(u => u.Id == id);
+//}).WithName("GetCoupon").Produces<Coupon>(200);
 
-app.MapPost("/api/coupon", ([FromBody] Coupon coupon) => {
-    if(coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+app.MapPost("/api/coupon", ([FromBody] CouponCreateDTO coupon_C_DTO) =>
+{
+    if (coupon_C_DTO.Id != 0 || string.IsNullOrEmpty(coupon_C_DTO.Name))
     {
         return Results.BadRequest("Invalis Id or Coupon name");
     }
-    if(CouponStore.)
-});
+    if (CouponStore.couponList.FirstOrDefault(u => u.Name.ToLower() == coupon_C_DTO.Name.ToLower()))
+    {
+        return Results.BadRequest("Coupon Name already exists!");
+    }
+
+    Coupon coupon = new()
+    {
+        IsActive = coupon_C_DTO.IsActive,
+        Name = coupon_C_DTO.Name,
+        Percent = coupon_C_DTO.Percent
+    };
+
+    coupon.Id = CouponStore.couponList.OrderByDescending(u => u.Id).FirstOrDefault().Id;
+    couponStore.couponList.Add(coupon);
+    CouponDTO couponDTO = new()
+    {
+        Id = coupon.Id,
+        IsActive = coupon.IsActive,
+        Name = coupon.Name,
+        Percent = coupon.Percent,
+        Created = coupon.Created
+    };
+    return Results.CreatedAtRoute("GetCoupon", new { id = coupon.Id }, coupon);
+}).WithName("CreateCoupon").Accepts<CouponCreateDTO>("application/json").Produces<Coupon>(201).Produces<Coupon>(400);
 
 app.UseHttpsRedirection();
 
