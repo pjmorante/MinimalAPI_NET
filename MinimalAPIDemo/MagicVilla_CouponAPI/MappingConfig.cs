d68f546df087a8dc76d2e069820434e77f249c56
Pedro Morante <pedro.morante@outlook.com>
2023-02-01 21:35:15 -0500

    Implement IRepository interface

--- MinimalAPIDemo/MagicVilla_CouponAPI/MappingConfig.cs
+++ MinimalAPIDemo/MagicVilla_CouponAPI/MappingConfig.cs
@@ -9,7 +9,7 @@ namespace MagicVilla_CouponAPI
         public MappingConfig()
         {
             CreateMap<Coupon, CouponCreateDTO>().ReverseMap();
-            //CreateMap<Coupon, CouponUpdateDTO>().ReverseMap();
+            CreateMap<Coupon, CouponUpdateDTO>().ReverseMap();
             CreateMap<Coupon, CouponDTO>().ReverseMap();
             //CreateMap<LocalUser, UserDTO>().ReverseMap();
             //CreateMap<ApplicationUser, UserDTO>().ReverseMap();
